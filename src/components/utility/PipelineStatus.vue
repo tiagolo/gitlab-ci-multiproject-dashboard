386e70860bd9481c5a890ac7f4901b11fbf34581
Tiago Lopes da Costa <tiago.costa@mv.com.br>
2018-04-12 09:24:13 -0300

    PipelineStatus icon and color parse from gitlab ci status

--- src/components/utility/PipelineStatus.vue
+++ src/components/utility/PipelineStatus.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-avatar :class="pipeline.status == 'success' ? 'green' : 'red' ">
-        <v-icon dark>{{ pipeline.status == 'success' ? 'check' : 'block'}}</v-icon>
+    <v-avatar :class="getPipelineStatusColor">
+        <v-icon dark>{{ getPipelineStatusIcon }}</v-icon>
     </v-avatar>
 </template>
 <script>
@@ -8,6 +8,36 @@ export default {
   name: 'PipelineStatus',
   props: {
     pipeline: {},
+  },
+  computed: {
+    getPipelineStatusColor() {
+      switch (this.pipeline.status) {
+        case 'success':
+          return 'green';
+        case 'failed':
+          return 'red'
+        case 'pending':
+          return 'orange'
+        case 'running':
+          return 'blue'
+        default:
+          return 'grey darken-1';
+      }
+    },
+    getPipelineStatusIcon() {
+      switch (this.pipeline.status) {
+        case 'success':
+          return 'check';
+        case 'failed':
+          return 'block'
+        case 'pending':
+          return 'pause'
+        case 'running':
+          return 'play_arrow'
+        default:
+          return 'remove';
+      }
+    },
   },
 };
 </script>
